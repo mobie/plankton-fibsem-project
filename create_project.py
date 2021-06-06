c428889d47547bdbb6c3aa525776d295c15ba863
Constantin Pape <constantin.pape@embl.de>
2021-06-06 22:45:40 +0200

    Update all datasets

--- create_project.py
+++ create_project.py
@@ -17,10 +17,12 @@ chunks = (64, 64, 64)
 #
 
 
-def copy_data(path, ds_name, name, invert=False):
+def copy_data(path, ds_name, name, invert=False, binarize=False):
     data, header = nrrd.read(path, index_order='C')
     if invert:
         data = data.max() - data
+    if binarize:
+        data = (data > 0).astype(data.dtype)
     vrange = [data.min(), data.max()]
     resolution = np.diag(header['space directions'])[::-1] / 1000.
     tmp_path = f'./tmp_data/{ds_name}'
@@ -33,9 +35,9 @@ def copy_data(path, ds_name, name, invert=False):
     return tmp_path, resolution.tolist(), vrange
 
 
-def add_data(path, ds_name, name, menu, invert=False):
-    tmp_path, resolution, vrange = copy_data(path, ds_name, name, invert=invert)
-    view = mobie.metadata.get_default_view('image', name, contrastLimits=vrange)
+def add_data(path, ds_name, name, menu, invert=False, binarize=False, opacity=1.0):
+    tmp_path, resolution, vrange = copy_data(path, ds_name, name, invert=invert, binarize=binarize)
+    view = mobie.metadata.get_default_view('image', name, contrastLimits=vrange, opacity=opacity)
     mobie.add_image(tmp_path, "data", "./data", ds_name,
                     image_name=name, resolution=resolution,
                     scale_factors=scale_factors, chunks=chunks,
@@ -51,10 +53,7 @@ def get_organelles(name, cell_id):
     organelle_names = []
     for path in all_paths:
         name = os.path.splitext(os.path.split(path)[1])[0]
-        if name.startswith('Cell-coccolith'):
-            organelle_paths.append(path)
-            organelle_names.append('coccolith')
-        elif name.startswith('Cell'):
+        if name.startswith('Cell') or name.startswith('Starch') or name.startswith('Pyrenoid'):
             continue
         else:
             organelle_paths.append(path)
@@ -88,18 +87,62 @@ def copy_segmentations(paths, ds_name, seg_name):
     return tmp_path, resolution.tolist()
 
 
-def add_segmentations(paths, class_names, ds_name, seg_name, menu):
+def add_segmentations(paths, class_names, ds_name, seg_name, menu, opacity=0.5):
     assert len(paths) == len(class_names)
     tmp_path, resolution = copy_segmentations(paths,  ds_name, seg_name)
+    view = mobie.metadata.get_default_view('segmentation', seg_name, opacity=opacity)
     mobie.add_segmentation(tmp_path, 'data', './data', ds_name,
                            segmentation_name=seg_name, resolution=resolution,
                            scale_factors=scale_factors, chunks=chunks,
-                           menu_name=menu, target='local', max_jobs=16)
+                           menu_name=menu, target='local', max_jobs=16,
+                           view=view)
     # add semantic column
     table_path = f'./data/{ds_name}/tables/{seg_name}/default.tsv'
     assert os.path.exists(table_path)
     table = pd.read_csv(table_path)
     table["organelle"] = class_names
+    table.to_csv(table_path, sep='\t', index=False)
+
+
+def _add_grid_view(ds_name, have_pyr, have_starch):
+    cell_ids = (1, 2, 3)
+
+    def _get_sources(cell_id):
+        sources = [f'cell{cell_id}-raw', f'cell{cell_id}-mask', f'cell{cell_id}-organelles']
+        if have_pyr:
+            sources += [f'cell{cell_id}-pyrenoid-mask']
+        if have_starch:
+            sources += [f'cell{cell_id}-starch-mask']
+        return sources
+
+    sources = [_get_sources(cell_id) for cell_id in cell_ids]
+    display_groups = {f'cell{cell_id}-raw': 'raw' for cell_id in cell_ids}
+    display_groups.update(
+        {f'cell{cell_id}-mask': 'cell-masks' for cell_id in cell_ids}
+    )
+    display_groups.update(
+        {f'cell{cell_id}-organelles': 'organelles' for cell_id in cell_ids}
+    )
+    if have_pyr:
+        display_groups.update(
+            {f'cell{cell_id}-pyrenoid-mask': 'pyrenoid-masks' for cell_id in cell_ids}
+        )
+    if have_starch:
+        display_groups.update(
+            {f'cell{cell_id}-starch-mask': 'starch-masks' for cell_id in cell_ids}
+        )
+    mobie.metadata.add_grid_bookmark(
+        dataset_folder=f'./data/{ds_name}',
+        name='all-cells',
+        sources=sources,
+        display_groups=display_groups,
+        overwrite=True
+    )
+
+    # fix the grid view table
+    table_path = os.path.join('data', ds_name, 'tables', 'all-cells', 'default.tsv')
+    table = pd.read_csv(table_path, sep='\t')
+    table['cell'] = np.arange(1, len(table) + 1)
     table.to_csv(table_path, sep='\t', index=False)
 
 
@@ -109,6 +152,7 @@ def create_split_dataset(name):
         print("Skipping", name)
         return
 
+    have_pyr, have_starch = False, False
     for cell_id in (1, 2, 3):
 
         # add the em data
@@ -120,33 +164,27 @@ def create_split_dataset(name):
         # add the cell mask
         seg_path = os.path.join(root, name, f'Data{cell_id}/Segmented-files/Cell{cell_id}.nrrd')
         seg_name = f'cell{cell_id}-mask'
-        add_data(seg_path, ds_name, seg_name, menu='segmentation')
+        add_data(seg_path, ds_name, seg_name, menu='segmentation', binarize=True, opacity=0.5)
+
+        # add additional masks depending on the dataset
+        if name in ('Micromonas-cell', 'Phaeodactylum-cell'):
+            seg_path = os.path.join(root, name, f'Data{cell_id}/Segmented-files/Pyrenoid{cell_id}.nrrd')
+            seg_name = f'cell{cell_id}-pyrenoid-mask'
+            add_data(seg_path, ds_name, seg_name, menu='segmentation', binarize=True, opacity=0.5)
+            have_pyr = True
+        if name == 'Micromonas-cell':
+            seg_path = os.path.join(root, name, f'Data{cell_id}/Segmented-files/Starch{cell_id}.nrrd')
+            seg_name = f'cell{cell_id}-starch-mask'
+            add_data(seg_path, ds_name, seg_name, menu='segmentation', binarize=True, opacity=0.5)
+            have_starch = True
 
         # add the organelle segmentations
         organelle_paths, organelle_names = get_organelles(name, cell_id)
         assert len(organelle_names) == len(organelle_paths)
-        print(organelle_names)
         seg_name = f'cell{cell_id}-organelles'
-        add_segmentations(organelle_paths, organelle_names, ds_name, seg_name, menu='segmentation')
+        add_segmentations(organelle_paths, organelle_names, ds_name, seg_name, menu='segmentation', opacity=0.25)
 
-    # add grid view
-    cell_ids = (1, 2, 3)
-    sources = [[f'cell{cell_id}-raw', f'cell{cell_id}-mask', f'cell{cell_id}-organelles']
-               for cell_id in cell_ids]
-    display_groups = {f'cell{cell_id}-raw': 'raw' for cell_id in cell_ids}
-    display_groups.update(
-        {f'cell{cell_id}-mask': 'cell-masks' for cell_id in cell_ids}
-    )
-    display_groups.update(
-        {f'cell{cell_id}-organelles': 'organelles' for cell_id in cell_ids}
-    )
-    mobie.metadata.add_grid_bookmark(
-        dataset_folder=f'./data/{ds_name}',
-        name='all-cells',
-        sources=sources,
-        display_groups=display_groups,
-        overwrite=True
-    )
+    _add_grid_view(ds_name, have_pyr, have_starch)
 
 
 def create_split_project():
@@ -181,11 +219,11 @@ def check_shapes(name):
             assert np.array_equal(shape, this_shape)
 
 
-def add_merged_cell_mask(name, ds_name):
+def add_merged_cell_mask(name, ds_name, semantic_name='Cell', seg_name='cell-mask'):
     root_ds = os.path.join(root, name)
     data = None
     for cell_id in (1, 2, 3):
-        path = os.path.join(root_ds, f'Data{cell_id}/Segmented-files/Cell{cell_id}.nrrd')
+        path = os.path.join(root_ds, f'Data{cell_id}/Segmented-files/{semantic_name}{cell_id}.nrrd')
         this_data, header = nrrd.read(path, index_order='C')
         resolution = (np.diag(header['space directions'])[::-1] / 1000.).tolist()
 
@@ -194,11 +232,11 @@ def add_merged_cell_mask(name, ds_name):
         else:
             data[this_data > 0] = cell_id
 
-    tmp_path = f'./tmp_data/{name}/merged_cell_masks.n5'
+    tmp_path = f'./tmp_data/{name}/merged_{seg_name}.n5'
     with z5py.File(tmp_path, 'w') as f:
         f.create_dataset('data', data=data, compression='gzip', chunks=chunks, n_threads=8)
     mobie.add_segmentation(tmp_path, 'data', './data', ds_name,
-                           segmentation_name="cell-mask", resolution=resolution,
+                           segmentation_name=seg_name, resolution=resolution,
                            scale_factors=scale_factors, chunks=chunks,
                            menu_name="segmentation", target='local', max_jobs=16)
 
@@ -260,6 +298,9 @@ def create_merged_dataset(name):
     add_data(raw_path, ds_name, name='raw', menu='fibsem', invert=True)
 
     add_merged_cell_mask(name, ds_name)
+    # add the cocolith mask (only for Emiliania)
+    if name == 'Emiliania-cell':
+        add_merged_cell_mask(name, ds_name, semantic_name='Cell-coccolith', seg_name='cocolith-mask')
 
     add_merged_organelles(name, ds_name)
 
@@ -275,7 +316,7 @@ def create_merged_project():
         create_merged_dataset(merged_set)
 
 
-# TODO add data for the remote s3 store and upload it
+# TODO add metadata for the remote s3 store and upload it
 if __name__ == '__main__':
-    create_merged_project()
+    # create_merged_project()
     create_split_project()
